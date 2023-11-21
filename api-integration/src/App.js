702fe8fee2b8057454019e477b28198ddff078e1
utkarsh-1602 <usking619@gmail.com>
2023-11-21 20:01:41 +0530

    api-integration folder

---
+++ api-integration/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
