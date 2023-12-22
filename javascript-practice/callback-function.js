2e01da6f3832bc447484cfa2bd32d705ebf1d275
utkarsh-1602 <usking619@gmail.com>
2023-12-22 18:20:38 +0530

    callback function updated

--- javascript-practice/callback-function.js
+++ javascript-practice/callback-function.js
@@ -29,3 +29,16 @@ function processData2() {
 
 fetchData2().then(processData2);
 
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+function fun1(callback) {
+    callback();
+}
+
+function fun2() {
+    console.log("This is a callback function")
+}
+
+fun1(fun2)
