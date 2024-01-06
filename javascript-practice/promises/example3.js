d7f902cb9cc8bf2a3d12b90f55fb57fa2a3d64c0
utkarsh-1602 <usking619@gmail.com>
2024-01-06 22:36:00 +0530

    example3.js promise updated

--- javascript-practice/promises/example3.js
+++ javascript-practice/promises/example3.js
@@ -1,6 +1,7 @@
 // Using Promise.all
 
 // Question : https://leetcode.com/problems/add-two-promises/?envType=study-plan-v2&envId=30-days-of-javascript
+// stackoverflow : https://stackoverflow.com/questions/31700528/combining-two-promises
 
 var addTwoPromises = async function(promise1, promise2) {
     var v;
@@ -10,7 +11,3 @@ var addTwoPromises = async function(promise1, promise2) {
     return v
 };
 
-/**
- * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
- *   .then(console.log); // 4
- */
