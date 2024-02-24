239d086416082d47d16a0c9027be3584b12c102a
utkarsh-1602 <usking619@gmail.com>
2024-02-25 00:58:56 +0530

    url handling nodejs

--- nodejs/url-handling/index.js
+++ nodejs/url-handling/index.js
@@ -1,20 +1,32 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
     if(req.url === '/favicon.ico') return res.end() 
     const ipAddress = req.socket.remoteAddress;
     const log = `${Date.now()}: New request received from ${ipAddress} ${req.url}\n`;
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl)
     
     fs.appendFile('log.txt', log, () => {
 
-        switch(req.url){
+        switch(myUrl.pathname){
+
             case '/':
                 res.end('Homepage')
                 break
+
             case '/about':
-                res.end('About page')
+                const username = myUrl.query.name;
+                res.end(`Hi, ${username}`)
                 break
+
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end(`Here are your search results for ${search}`)
+                break
+
             default:
                 res.end('404 Not Found')
         }
