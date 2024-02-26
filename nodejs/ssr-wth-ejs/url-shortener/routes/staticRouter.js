50564ba3fb755750b24c1009a6dd2b5d012d7507
utkarsh-1602 <usking619@gmail.com>
2024-02-26 14:27:12 +0530

    ssr with ejs completed

--- nodejs/ssr-wth-ejs/url-shortener/routes/staticRouter.js
+++ nodejs/ssr-wth-ejs/url-shortener/routes/staticRouter.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const URL = require('../models/url');
 const router = express.Router();
 
 router.get('/test', async(req,res) => {
-    return res.render('home')
+    const allurls = await URL.find({});
+    console.log(allurls)
+    return res.render('home', {
+        allurls: allurls
+    })
 })
 
 module.exports = router;
